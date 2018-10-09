4350cc7144afadbfc948fd1f2f55acd935614c45
Otavio Augusto <tavioalves@gmail.com>
2018-10-08 23:03:18 -0300

    docs: Exclusão de arquivos desnecessários e renomeação de App.js para MyReads.js

---
+++ src/MyReads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';;
+
+class MyReads extends Component {
+  render() {
+    return (
+      <div>
+        <p>My reads</p>
+      </div>
+    );
+  }
+}
+
+export default MyReads;
