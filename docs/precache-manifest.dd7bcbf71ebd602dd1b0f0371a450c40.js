5eb685ffd688919d2adcca46aae032d6573663df
Otavio Augusto <tavioalves@gmail.com>
2018-10-10 19:08:56 -0300

    chore: Criação da pasta docs para produção

---
+++ docs/precache-manifest.dd7bcbf71ebd602dd1b0f0371a450c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "ccaad7cbef00b0c73057",
+    "url": "/my-reads/static/js/main.ccaad7cb.chunk.js"
+  },
+  {
+    "revision": "1c3aea05ea9fb952d7ef",
+    "url": "/my-reads/static/js/1.1c3aea05.chunk.js"
+  },
+  {
+    "revision": "ccaad7cbef00b0c73057",
+    "url": "/my-reads/static/css/main.aa1ca742.chunk.css"
+  },
+  {
+    "revision": "1c3aea05ea9fb952d7ef",
+    "url": "/my-reads/static/css/1.3c03313d.chunk.css"
+  },
+  {
+    "revision": "67db90ec0f54f29244131d470cac8166",
+    "url": "/my-reads/index.html"
+  }
+];
