2f97ee26fe6f21c6b6bfc599b84e8da2aa04c724
Otavio Augusto <otavio.augusto@geekhunter.com.br>
2018-10-26 16:57:49 -0300

    Updates

---
+++ precache-manifest.a9f0e1d1f6170a22def85f0e328f1fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "9d582a670296c60e65f4",
+    "url": "/my-reads/static/js/main.9d582a67.chunk.js"
+  },
+  {
+    "revision": "3bfe74821e9b85eb26be",
+    "url": "/my-reads/static/js/1.3bfe7482.chunk.js"
+  },
+  {
+    "revision": "9d582a670296c60e65f4",
+    "url": "/my-reads/static/css/main.3b2da535.chunk.css"
+  },
+  {
+    "revision": "3bfe74821e9b85eb26be",
+    "url": "/my-reads/static/css/1.53348597.chunk.css"
+  },
+  {
+    "revision": "0050960d8388a8065e25e6a9e2db7433",
+    "url": "/my-reads/index.html"
+  }
+];
